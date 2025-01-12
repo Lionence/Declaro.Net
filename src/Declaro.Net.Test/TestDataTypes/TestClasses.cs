d53ebfe780936b4a37d9e210127b15928cb53015
Dániel Nagy <daniel.nagy@lionence.com>
2025-01-12 16:51:38 +0100

    Remove unnecessary test class

--- src/Declaro.Net.Test/TestDataTypes/TestClasses.cs
+++ src/Declaro.Net.Test/TestDataTypes/TestClasses.cs
@@ -37,32 +37,3 @@ public sealed class WeatherRequestResponse : IWeatherResponse, IWeatherRequest
     [RequestArgument(1)]
     public required string? Date { get; set; }
 }
-
-[HttpGet(ApiEndpoint = "umbraco/delivery/api/v2/content/item/{0}", CacheTime = "10:00")]
-public class Website
-{
-    public string ContentType { get; set; }
-    public string Name { get; set; }
-
-    [RequestArgument(0)]
-    public Guid Id { get; set; }
-    public string ErrorNotFoundHeader { get; set; }
-    public string ErrorNotFoundDescription { get; set; }
-    public string ErrorNotFoundButtonText { get; set; }
-    public string ErrorNotFoundButtonHref { get; set; }
-    public string ErrorOtherHeader { get; set; }
-    public string ErrorOtherDescription { get; set; }
-    public string ErrorOtherButtonText { get; set; }
-    public string ErrorOtherButtonHref { get; set; }
-    public string BaseTitle { get; set; }
-    public List<Favicon> Favicon { get; set; }
-    public string Description { get; set; }
-    public string Keywords { get; set; }
-    public string SeoRobotsMetaTag { get; set; }
-}
-
-public class Favicon
-{
-    public Guid Id { get; set; }
-    public string Url { get; set; }
-}
